3218cb087ec2f160f8a2bbd582ebd509307cff66
Ben Eggers <beggerss@cs.washington.edu>
2014-06-15 18:47:44 -0700

    wrote solve

--- sudoku.rs
+++ sudoku.rs
@@ -24,6 +24,82 @@ impl Board {
 		self.nums[row][col] = num;
 	}
 
+	// Prints the board to stdout
+	fn print(&self) {
+		for col in range(0u, 9u) {
+			for row in range(0u, 9u) {
+				let n = self.get(row, col);
+				if n != 0 {
+					print!("{} ", n)
+				} else {
+					// We want dashes, not 0s
+					print!("- ")
+				}
+				if (row+1) % 3 == 0 {
+					// block spacing
+					print!(" ")
+				}
+			}
+			println!("")
+			if (col+1) % 3 == 0 {
+				// block spacing
+				println!("")
+			}
+		}
+	}
+
+	// Solves the board. Returns true if the board can be solved and false
+	// otherwise. If true was returned, the board will be in a solved state. If
+	// false, the board will be in the state it was in when this method was
+	// called.
+	fn solve(&mut self) -> bool {
+		// We're going to brute-force recursive-backtrack all the possible
+		// combinations (yay NP-complete problems)
+		let next = self.next_open_square();
+		let next_x = next.val0();
+		let next_y = next.val1();
+
+		// Check to see if the board is complete--if it is, we're done!
+		if next == (0, 0) {
+			return true
+		}
+
+		for num in range(0u8, 9u8) {
+			if self.is_legal(next_x, next_y, num) {
+				// We're allowed to put this number there, so do so
+				self.set(next_x, next_y, num);
+
+				// Then recurse to see if the board is solvable
+				let solved = self.solve();
+				if solved {
+					// We did it!
+					return true;
+				}
+
+				// Nope, keep trying
+			}
+		}
+		
+		// Couldn't solve it. Reset the considered square to 0 and return false
+		self.set(next_x, next_y, 0);
+		false
+	}
+
+	// Returns the next zero (row, col) on the sudoku board. "Next" is defined
+	// as top coming before bottom, left coming before right. (0, 0) will be
+	// returned if there are no open squares.
+	fn next_open_square(&self) -> (uint, uint) {
+		 for col in range(0u, 9u) {
+		 	for row in range(0u, 9u) {
+		 		if self.get(row, col) == 0 {
+		 			return (row, col)
+		 		}
+		 	}
+		 }
+
+		(0, 0)
+	}
+
 	// Whether or not the passed number is legal at (row, col)
 	fn is_legal(&self, row: uint, col: uint, num: u8) -> bool {
 		// If the number is already there, it's possible to have it there
@@ -39,7 +115,7 @@ impl Board {
 		}
 
 		// Now, check its "box" of 9 points
-		let box_pt_r = row / 3 * 3;  // Yay integer arithmetic!
+		let box_pt_r = row / 3 * 3;  // Woo, integer arithmetic!
 		let box_pt_c = col / 3 * 3;
 		for r in range(0u, 3u) {
 			for c in range(0u, 3u) {
@@ -51,30 +127,6 @@ impl Board {
 
 		true
 	}
-
-	// Prints the board to stdout
-	fn print(&self) {
-		for row in range(0u, 9u) {
-			for col in range(0u, 9u) {
-				let n = self.get(row, col);
-				if n != 0 {
-					print!("{} ", n)
-				} else {
-					// We want dashes, not 0s
-					print!("- ")
-				}
-				if (col+1) % 3 == 0 {
-					// block spacing
-					print!(" ")
-				}
-			}
-			println!("")
-			if (row+1) % 3 == 0 {
-				// block spacing
-				println!("")
-			}
-		}
-	}
 }
 
 
@@ -82,5 +134,6 @@ fn main() {
 	let mut b = box Board::new();
 	b.print();
 	b.set(0, 0, 1);
+	b.set(4, 0, 2);
 	b.print();
 }
